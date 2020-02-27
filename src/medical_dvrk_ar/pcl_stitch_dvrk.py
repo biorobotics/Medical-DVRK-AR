efa43ae927164a88a4c049e7e435796bbbc189d4
KoalalikeTree <43040078+KoalalikeTree@users.noreply.github.com>
2020-02-26 19:37:37 -0500

    add publisher
    stitch the point cloud in robot frame

--- src/medical_dvrk_ar/pcl_stitch_dvrk.py
+++ src/medical_dvrk_ar/pcl_stitch_dvrk.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-"""Don't test, it's just a draft :P"""
-
 # system
 import rospy
 
@@ -13,7 +11,6 @@ import numpy as np
 from sensor_msgs.msg import PointCloud2, PointField
 from sensor_msgs import point_cloud2
 from std_msgs.msg import Header
-
 
 # tf
 import tf
@@ -31,11 +28,19 @@ The main reference includes:
 1. (ros listener) http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
 2. (read pointcloud2) https://answers.ros.org/question/240491/point_cloud2read_points-and-then/
 3. (encode pointcloud2) https://gist.github.com/lucasw/ea04dcd65bc944daea07612314d114bb#file-create_cloud_xyzrgb-py-L41
+
+To run this code, you need at first:
+1. Link and run all blaser launch files
+2. Run broadcaster for blaser (TODO: compile launch file)
+
+After running this file, you should see:
+1. A topic named "organ_3d_point_cloud" publish all the 3d pcl
 	
 TODO:
 1. write a lauch file to do the broadcast and stitching at the same time
+2. how to visualize it in rviz?
 Author - Cora Zhang
-Date - Feb 20
+Date - Feb 26
 """
 
 class stiching_3d_pcl:
@@ -46,17 +51,18 @@ class stiching_3d_pcl:
 		#initialize a node for pcl stitching
 		rospy.init_node('pcl_stitcher', anonymous=True)
 
-		#publish the stitched 3d pcl to the  "organ_rd_point_cloud" topic
+		# for listening the robot-blaser tf
 		self.tflistener = tf.TransformListener()
+		# publish the stitched 3d pcl to the  "organ_rd_point_cloud" topic
 		self.pub = rospy.Publisher("organ_3d_point_cloud", PointCloud2, queue_size=2)
 
 		# for encode the 3d_pcl
 		self.fields = [PointField('x', 0, PointField.FLOAT32, 1),
-				  PointField('y', 4, PointField.FLOAT32, 1),
-				  PointField('z', 8, PointField.FLOAT32, 1)]
+					  PointField('y', 4, PointField.FLOAT32, 1),
+					  PointField('z', 8, PointField.FLOAT32, 1)]
 		self.header = Header()
-		self.header.frame_id = "organ_3d_ptc" # the 3dpcl is in a new frame
-		self.pc2 = point_cloud2.create_cloud(header, fields, self.cloud_points)
+		self.header.frame_id = "blaser" # the 3dpcl is in a new frame
+		self.pc2 = point_cloud2.create_cloud(self.header, self.fields, self.cloud_points)
 
 	def blaser_listener(self):
 		"""This listener listen to 'pointcloud2 msg' from the 'blaser_pcl_topic',
@@ -69,7 +75,11 @@ class stiching_3d_pcl:
 		It mainly:
 		1. read the x,y,z of points from pointcloud2
 		2. listen to the tf of world2blaser
-		and save xyz of all the points in a list"""
+		3. get transform matrix from 2. and apply it on 1. to get 3d point cloud
+		4. save all received point in one list
+		5. encode the list into ros pointcloud2 data
+		6. publish the pointcloud2 data to "organ_3d_point_cloud" topic
+		"""
 
 		# in blaser the z is the height, x is the position, since the data is in 2D, so y has no meaning
 		gen = point_cloud2.read_points(data, field_names = ("x", "y", "z"), skip_nans=True)
@@ -92,19 +102,20 @@ class stiching_3d_pcl:
 			transform[0:3, -1] = trans # assign the translation vector tot he transform matrix
 
 			point_world_frame = np.dot(transform, point_homo) # transform the point to world frame using transform matrix
-			print(point_world_frame)
+			# print(point_world_frame)
 
+			# # TODO: test
 			self.cloud_points.append([point_world_frame[0],point_world_frame[1],point_world_frame[2]])# store every point in the list
-			# print (" x : %.4f  y: %.4f  z: %.4f" %(p[0],p[1],p[2]))
+			# # print (" x : %.4f  y: %.4f  z: %.4f" %(p[0],p[1],p[2]))
 
 			self.pc2 = point_cloud2.create_cloud(self.header, self.fields, self.cloud_points) # create the 3dpcl for publish
+
+			pcl_stitcher.pc2.header.stamp = rospy.Time.now()
+			pcl_stitcher.pub.publish(pcl_stitcher.pc2)
+
+			# print(self.pc2)
 
 if __name__ == "__main__":
 	pcl_stitcher = stiching_3d_pcl()
 	pcl_stitcher.blaser_listener()
-
-	while not rospy.is_shutdown():
-		pcl_stitcher.pc2.header.stamp = rospcreate_cloudy.Time.now()
-		pcl_stitcher.pub.publish(pc2)
-		pcl_stitcher.rospy.sleep(1.0)
 	
