91361e1c0155e673088530b855795ae4c2383b17
ArtiAnantharaman <57013228+ArtiAnantharaman@users.noreply.github.com>
2020-03-22 00:21:29 -0400

    Update scan_liver_no_wrist_movement.py

--- src/medical_dvrk_ar/scan_liver_no_wrist_movement.py
+++ src/medical_dvrk_ar/scan_liver_no_wrist_movement.py
@@ -18,8 +18,8 @@ def main():
 	robot.home()
 	position_start = robot.get_current_position()
 	safe_pos = PyKDL.Frame( PyKDL.Rotation(PyKDL.Vector(0, 1, 0),
-										   PyKDL.Vector(1, 0, 0),
-										   PyKDL.Vector(0, 0,-1)), 
+						    PyKDL.Vector(1, 0, 0),
+						    PyKDL.Vector(0, 0,-1)), 
 							PyKDL.Vector(0.05,-0.04,-0.13))
 	robot.move(safe_pos)
 	
