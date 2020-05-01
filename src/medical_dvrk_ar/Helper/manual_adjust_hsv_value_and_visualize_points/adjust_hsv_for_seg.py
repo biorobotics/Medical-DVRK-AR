4ae150a37dfd5b0b8405ec78948e49a2a8cfe2e6
cora <beileiz@andrew.cmu.edu>
2020-05-01 15:48:22 -0400

    add comment to hsv helper

--- src/medical_dvrk_ar/Helper/manual_adjust_hsv_value_and_visualize_points/adjust_hsv_for_seg.py
+++ src/medical_dvrk_ar/Helper/manual_adjust_hsv_value_and_visualize_points/adjust_hsv_for_seg.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 """
 Function:
-1. receive rosmsg_poincloud2 from specific topic, and save pcd file of two frame for registration and tracking
-Pre-requisite
-1. roslaunch realsense2_camera rs_camera.launch filters:=pointcloud
-Main reference (This code is not based on the most recent Open3d API)
-1. https://github.com/felixchenfy/open3d_ros_pointcloud_conversion/blob/master/lib_cloud_conversion_between_Open3D_and_ROS.py
-Param: (detail in convertAndSavePCD() function)
-1.topic_name: the topic of ros_msg:poindcloud2, default is for realsense
-2.time_interval: the time interval for recording the "target" and "source", for further registration and tracking
-3.save_directory: the directory to save pcd file
-Author: Cora
-March 30
+1. receive rosmsg_poincloud2 from realsens topic
+2. apply hsv filter on points
+3. publish segmented points for visualization in rviz
+
+Author: Alex
+May 1
+
 TODO:
-1. How to move the save npy out of the callback?
-2. Integrate Anjali's segmentation code
+1. Integrate it with the main pipeline
+2. add GUI for adjust hsv value
 """
 
 
@@ -65,10 +61,28 @@ class HSV_set():
         # self.pc2 = point_cloud2.create_cloud(self.header, self.FIELDS_XYZRGB, self.pointcloud)
        
     def hsv_points_filter(self, topic_name):
+        """
+        subsribe to the realsense point cloud topic and call the function
+
+        params:
+        topic_name: string, realsense topic for point cloud
+
+        """
         rospy.Subscriber(topic_name, PointCloud2, self.callback)
     
     def filter(self,ros_cloud):
-       
+        """
+        apply filter on the received point cloud
+
+        params:
+        ros_cloud: point cloud of ros_msg pointcloud2 datatype
+
+        return:
+        result: [N x 6] nd array, point cloud of only liver
+                N is the number of the qualified points
+                6 is x y z red blue green value
+
+        """
         # for realsense data
         field_names = ("x", "y", "z", "rgb")
 
@@ -123,49 +137,34 @@ class HSV_set():
         open3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
 
     def callback(self, ros_cloud):
+        """
+        will be called when received point cloud from realsense
+
+        params:
+        ros_cloud: point cloud of ros_msg pointcloud2 datatype
+
+        return:
+        publish the points in topic 'filtered_pointcloud'
+
+        """
         self.pointcloud = []
         # apply hsv filter on point cloud
         self.received_ros_cloud = ros_cloud
         self.open3d_pointcloud = self.filter(self.received_ros_cloud)
-        # self.filtered_pointcloud = point_cloud2.create_cloud(self.header, self.FIELDS_XYZRGB, self.received_ros_cloud) # create the 3dpcl for publish
-        # self.convertCloudFromOpen3dToRos(self.open3d_pointcloud)
-
 
         for itr in range (len(self.open3d_pointcloud)):
-            # self.open3d_pointcloud[itr][0], self.open3d_pointcloud[itr][3] =self.open3d_pointcloud[itr][3], self.open3d_pointcloud[itr][0] 
+
             self.pointcloud.append(self.open3d_pointcloud[itr][:3].tolist())
-            # colors = np.asarray(self.open3d_pointcloud[itr][3:])*255 # nx3 matrix
-            # colors = colors[0] * BIT_MOVE_16 +colors[1] * BIT_MOVE_8 + colors[2]   
-            # list_pc = self.open3d_pointcloud[itr][:3].tolist()
-            # list_pc.append(colors)
-            # self.pointcloud.append(list_pc)
-        
+
         self.pc2 = point_cloud2.create_cloud(self.header, self.FIELDS_XYZ, self.pointcloud)
 
 
-        # self.filtered_pointcloud = self.pointcloud
-
-        # remove outlier
-        # self.open3d_pointcloud= self.open3d_pointcloud.voxel_down_sample(voxel_size=0.001)
-        # print('pointcloud data num before',np.array(self.open3d_pointcloud.points).shape)
-
-        # remove points that have less that 250 neighbours in 0.1 unit
-        # cl, ind = self.open3d_pointcloud.remove_radius_outlier(nb_points=100, radius=1)
-
-        # self.display_inlier_outlier(self.open3d_pointcloud, ind)
-        # for vidualization, assign the self.open3d_pointcloud after visualization
-        # self.open3d_pointcloud = cl
-        # print('pointcloud data num',np.array(self.open3d_pointcloud.points).shape)
-        
-        #publish filtered pointcloud
         my_pcl_pcd_converter.pc2.header.stamp = rospy.Time.now()
         my_pcl_pcd_converter.pub.publish(my_pcl_pcd_converter.pc2)
 
 
 if __name__ == "__main__":
-    # set your directory path
-    # dir_path = "/home/alex/Documents/bagfile/ros_to_open3d/"
-    # An example of using the above class
+
     my_pcl_pcd_converter =HSV_set()
 
     my_pcl_pcd_converter.hsv_points_filter("camera/depth/color/points")    
