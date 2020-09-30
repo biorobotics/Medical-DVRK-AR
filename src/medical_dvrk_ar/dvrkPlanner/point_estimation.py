d1e55bad17dd14b80b46632952d2c39c1b4728a1
Alex Wu <chichiaw@andrew.cmu.edu>
2020-09-30 00:55:03 -0400

    fixed call by reference bug

--- src/medical_dvrk_ar/dvrkPlanner/point_estimation.py
+++ src/medical_dvrk_ar/dvrkPlanner/point_estimation.py
@@ -6,6 +6,7 @@ from tf_conversions import posemath
 import os.path
 
 # Exp: Predict the locations of given points in the future time  0 <= t <= self.time_frame.
+
 def estimation(data, amp, freq, sim_start_time, time):
     # Input:    1) data(PyDKL.Frame()): point location
     #           2) time: future time i
@@ -14,11 +15,28 @@ def estimation(data, amp, freq, sim_start_time, time):
     #           5) sim_start_time: simulationn start time
     #           6) time: future time
     # Output:   1) pose(PyKDL.Frame()): predicted point location
-
     pose = np.array([data.p[0],data.p[1],data.p[2]])
     run_time = time - sim_start_time
-    pose[2] = pose[2] + amp * math.sin(freq * run_time)
-    data.p = PyKDL.Vector(pose[0], pose[1], pose[2])
+    move = pose[2] + amp * math.sin(freq * run_time)
+
+    estimated_point = PyKDL.Frame()
+    estimated_point.p = PyKDL.Vector(pose[0], pose[1], move)
+    estimated_point.M = data.M
+    return estimated_point
+
+def estimation_numpy(data, amp, freq, sim_start_time, time):
+    # Input:    1) data(numpy.array): point location
+    #           2) time: future time i
+    #           3) amp: amplitude of the motion
+    #           4) freq: frequency of the motion
+    #           5) sim_start_time: simulationn start time
+    #           6) time: future time
+    # Output:   1) pose(numpy.array): predicted point location
+
+    pose_z = data[2]
+    run_time = time - sim_start_time
+    pose_Z = pose_z + amp * math.sin(freq * run_time)
+    data[2] = pose_z
     return data
 
 def make_PyKDL_Frame(point):
@@ -26,7 +44,6 @@ def make_PyKDL_Frame(point):
     pykdl_point.p = PyKDL.Vector(point[0],point[1],point[2])
     pykdl_point.M = PyKDL.Rotation.Quaternion(point[3],point[4],point[5],point[6])
     return pykdl_point
-    
 
 # Exp: Predict the position of a certain point by estimating a reached time and search in the 
 #      predicted table to obtain the location
