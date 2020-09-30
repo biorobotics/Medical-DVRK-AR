6bf0edfedd957c545a6dbbbda8f66959b51fb415
Alex Wu <chichiaw@andrew.cmu.edu>
2020-09-30 00:55:57 -0400

    motion compensation unit test

--- src/medical_dvrk_ar/dvrkPlanner/robot_motion.py
+++ src/medical_dvrk_ar/dvrkPlanner/robot_motion.py
@@ -6,9 +6,10 @@ import numpy as np
 from tf_conversions import posemath
 
 import os.path
-from point_estimation import estimation, make_PyKDL_Frame
+from point_estimation import estimation, make_PyKDL_Frame, estimation_numpy
 
 functionPath = os.path.dirname(os.path.realpath(__file__))
+
 
 
 def resolvedRates(config,currentPose,desiredPose):
@@ -87,24 +88,40 @@ class ControlServer(object):
         self.maxForce = 800 # Not sure of this?
         self.safeZ = .05 # Safe height above organ in meters
         self.normalDistance = 0.005 # Meters
-        self.toolOffset = 0.05
+        self.toolOffset = 0.0
         self.amp = amplitude
         self.freq = frequency
+
+        # testing estimation
+        self.start_time = rospy.Time.now().to_sec()
 
     def move(self,desiredPose, maxForce):
         # currentPose = self.robot.get_desired_position()
 
-
+        # print(desiredPose.p)
         currentPose = self.robot.get_current_position()
         measuredPose_previous = self.robot.get_current_position()
+
+        # # make a copy of the desiredPose
+        # desiredPose_tmp = PyKDL.Frame()
+        # pose = np.array([0.0,0.0,0.0])
+        # pose[0] = np.copy(desiredPose.p[0])
+        # pose[1] = np.copy(desiredPose.p[1])
+        # pose[2] = np.copy(desiredPose.p[2])
+        # rotation = np.copy(desiredPose.M)
+        # desiredPose_tmp.p = PyKDL.Vector(pose[0],pose[1],pose[2])
+        # desiredPose_tmp.M = desiredPose.M
+
         while not rospy.is_shutdown():
             # get current and desired robot pose (desired is the top of queue)
             # compute the desired twist "x_dot" from motion command
+            # print(desiredPose.p)
+            desiredPose_move = estimation(desiredPose, self.amp, self.freq, 0, rospy.Time.now().to_sec())
+            # print(desiredPose_move.p)
 
-            desiredPose = estimation(desiredPose, self.amp, self.freq, 0, rospy.Time.now().to_sec())
+            desiredPosition = desiredPose_move.p - desiredPose_move.M.UnitZ()*self.toolOffset
             
-            desiredPosition = desiredPose.p - desiredPose.M.UnitZ()*self.toolOffset
-            desiredPoseWithOffset = PyKDL.Frame(desiredPose.M, desiredPosition)
+            desiredPoseWithOffset = PyKDL.Frame(desiredPose_move.M, desiredPosition)
 
             xDotMotion = resolvedRates(self.resolvedRatesConfig,
                                        currentPose,
@@ -113,26 +130,30 @@ class ControlServer(object):
             
 
             if xDotMotion.vel.Norm() <= 0.001 and xDotMotion.rot.Norm() <= 0.1:
-                # print('target pose: ')
-                # print(currentPose)
                 break               
-            
             self.robot.move(currentPose, interpolate = False)
             self.rate.sleep()
 
 if __name__=="__main__":
     rospy.init_node('Control_server')
 
-    p1 = [0.1,0.02,-0.1,0.7071068,0.7071068,0,0]
-    p2 = [0.01,0.02,-0.15, 0.7071068,0.7071068,0,0]
-    p3 = [0.00,0.00,-0.2,0.7071068,0.7071068,0,0]
-    data = np.vstack((p1,p2,p3))
-    amplitude = 0
-    frequency = 0
+    # p3 = [-0.1 ,0.0, -0.1, 0.7071068,0.7071068,0,0]
+    # p4=  [-0.05,0.0, -0.1, 0.7071068,0.7071068,0,0]
+    # p5 = [0.0  ,0.0, -0.1, 0.7071068,0.7071068,0,0]
+    # p6 = [0.05 ,0.0, -0.1, 0.7071068,0.7071068,0,0]
+    # p7 = [0.1  ,0.0, -0.1, 0.7071068,0.7071068,0,0]
+    # p8 = [0.05  ,0.0, -0.1, 0.7071068,0.7071068,0,0]
+    # p9 = [0.0  ,0.0, -0.1, 0.7071068,0.7071068,0,0]
+    # data = np.vstack((p3,p4,p5,p6,p7,p8,p9))
+    # np.save('testdata.npy',data)
+    data = np.load('testdata.npy')
+    amplitude = 0.05 #0.05
+    frequency = 0.5 #0.1
     server = ControlServer(amplitude, frequency)
-    
+    # server.move(make_PyKDL_Frame(data[0]), server.maxForce)
 
     number_of_points = len(data)
     for itr in range(0, number_of_points):
+        print(itr)
         dest = make_PyKDL_Frame(data[itr])
         server.move(dest, server.maxForce)
