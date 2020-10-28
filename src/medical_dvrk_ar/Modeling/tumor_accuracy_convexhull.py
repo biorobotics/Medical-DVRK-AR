90789412633f8e4b9bfc3259816db5204adc12c1
Arti Anantharaman <artianantharaman@gmail.com>
2020-10-28 14:29:51 -0400

    Modified accuracy calculations + computed centroids

--- src/medical_dvrk_ar/Modeling/tumor_accuracy_convexhull.py
+++ src/medical_dvrk_ar/Modeling/tumor_accuracy_convexhull.py
@@ -5,6 +5,45 @@ from matplotlib import pyplot as plt
 from mpl_toolkits import mplot3d
 from scipy.spatial import ConvexHull
 
+'''
+Inputs: 1. ground truth stiffness map
+        2. palpation stiffness map
+
+Outputs: 1. Percentage of each tumor classified
+         2. Total healthy tissue misclassified
+         3. Distanc
+'''
+
+def compute_area(ground_truth_area, palpated_area,which_tumor):
+	if(palpated_area <= ground_truth_area):
+		tumor_acc = palpated_area*100/ground_truth_area
+	else:
+		diff_area = palpated_area - ground_truth_area
+		tumor_acc = (1 - diff_area/ground_truth_area)*100
+
+	print('Tumor {} has been classified with accuracy = {}%'.format(which_tumor, tumor_acc))
+
+def misclassified(gt1,gt2,gt3,palp1,palp2,palp3):
+	miss_area = 0.0
+	if(palp1 > gt1):
+		miss_area += (palp1-gt1)/gt1
+	if(palp2 > gt2):
+		miss_area += (palp2-gt2)/gt2
+	if(palp3 > gt3):
+		miss_area += (palp3-gt3)/gt3
+
+	print('Total healthy tissue misclassified = {}%'.format(miss_area*100))
+
+def compute_centroid(arr):
+    length = arr.shape[0]
+    sum_x = np.sum(arr[:, 0])
+    sum_y = np.sum(arr[:, 1])
+    return np.array([sum_x/length, sum_y/length])
+
+def distance_betw_centroids(gt_centroid,palp_centroid):
+	dist = np.sqrt((gt_centroid[0]-palp_centroid[0])**2 + (gt_centroid[1]-palp_centroid[1])**2)
+	return dist
+
 def compute_accuracy(ground_truth_stiffness, palpated_points):
 	# GROUND TRUTH
 	# Get only (x,y) coordinates => calculate convex hull and accuracy in 2d
@@ -58,14 +97,29 @@ def compute_accuracy(ground_truth_stiffness, palpated_points):
 	palp3_area = hull_palp_3.volume
 
 	# Compute accuracy
-	t1_acc = palp1_area*100/gt1_area
-	t2_acc = palp2_area*100/gt2_area
-	t3_acc = palp3_area*100/gt3_area
+	compute_area(gt1_area,palp1_area,1)
+	compute_area(gt2_area,palp2_area,2)
+	compute_area(gt3_area,palp3_area,3)
+	# Compute missclassification
+	misclassified(gt1_area,gt2_area,gt3_area,palp1_area,palp2_area,palp3_area)
 
-	# Display tumor accuracy
-	print('Tumor {} has been classified with accuracy = {}%'.format(1, t1_acc))
-	print('Tumor {} has been classified with accuracy = {}%'.format(2, t2_acc))
-	print('Tumor {} has been classified with accuracy = {}%'.format(3, t3_acc))
+	# Get centroids of ground truth tumors
+	gt1_centroid = compute_centroid(gt1)
+	gt2_centroid = compute_centroid(gt2)
+	gt3_centroid = compute_centroid(gt3)
+
+	# Get centroids palpated tumors
+	palp1_centroid = compute_centroid(palp_t1)
+	palp2_centroid = compute_centroid(palp_t2)
+	palp3_centroid = compute_centroid(palp_t3)
+
+	t1_centroid_dist = distance_betw_centroids(gt1_centroid, palp1_centroid)
+	t2_centroid_dist = distance_betw_centroids(gt2_centroid, palp2_centroid)
+	t3_centroid_dist = distance_betw_centroids(gt3_centroid, palp3_centroid)
+
+	print('Distance between ground truth and estimated centroids of tumor {} = {}'.format(1, t1_centroid_dist))
+	print('Distance between ground truth and estimated centroids of tumor {} = {}'.format(2, t2_centroid_dist))
+	print('Distance between ground truth and estimated centroids of tumor {} = {}'.format(3, t3_centroid_dist))
 
 if __name__ == '__main__':
 	
