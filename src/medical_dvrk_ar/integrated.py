6f8bbe5804d8599bd93cae4abbf1c91fafca88e7
anjali <anjali.pemmaraju@gmail.com>
2020-11-16 14:34:18 -0800

    Integration using newest planner

--- src/medical_dvrk_ar/integrated.py
+++ src/medical_dvrk_ar/integrated.py
@@ -10,35 +10,35 @@ import numpy as np
 # im not sure if these are the right files right now because they're all named something different...
 file_path = "/home/anjalipemmaraju/catkin_ws/src/Medical-DVRK-AR/data/"
 
-# print("loading stiffness data")
-# stiffnessMap = stiffnessMap(file_path+"xyz_for_stiffness_est.npy", 'h')
-# stiffnessMap.getStiffnessMap()
+print("loading stiffness data")
+stiffnessMap = stiffnessMap(file_path+"xyz_for_stiffness_est.npy", 'h')
+stiffnessMap.getStiffnessMap()
 
-# print("loading scanning path")
-# data = np.load(file_path+"scanning_path_200_points.npy")
-# # # turn this into argparse so that we can change amp and freq easily
+print("loading scanning path")
+data = np.load(file_path+"scanning_path_200_points.npy")
+# # turn this into argparse so that we can change amp and freq easily
 amplitude = 0.02 #0.02
 frequency = 0.5 #0.5
 
-# finished = False
+finished = False
 
-# print("starting scan")
-# scan_planner = Task_Planner(data, frequency, amplitude)
-# finished = scan_planner.run()
-# print("finished scan")
-# # # open blaser results and do pointcloud palpation filter somehow
+print("starting scan")
+scan_planner = Task_Planner(data, frequency, amplitude)
+finished = scan_planner.run()
+print("finished scan")
+# # open blaser results and do pointcloud palpation filter somehow
 
-# print("processing blaser results")
-# blaser_data_name = "blaser_results.npy"
-# raw_data = np.load(file_path+blaser_data_name)
-# max_angle = 60  # change  the param within [0,90)]
-# keepRows = 50
-# keepCols = 50
+print("processing blaser results")
+blaser_data_name = "blaser_results.npy"
+raw_data = np.load(file_path+blaser_data_name)
+max_angle = 60  # change  the param within [0,90)]
+keepRows = 200
+keepCols = 100
 
-# my_filter = filter_pointcloud_for_path_planner(max_angle, keepRows, keepCols, connectivity_test=True)
-# a = my_filter.filter(raw_data, isPlyPath=False)
-# np.save(file_path+"palpation_path_36cols_36rows.npy", a)
-data = np.load(file_path + "palpation_path_36cols_36rows.npy")
+my_filter = filter_pointcloud_for_path_planner(max_angle, keepRows, keepCols, connectivity_test=True)
+a = my_filter.filter(raw_data, isPlyPath=False)
+np.save(file_path+"palpation_path_testing.npy", a)
+data = np.load(file_path + "palpation_path_testing.npy")
 
 print("beginning palpation")
 planner = Task_Planner_palpation(data, frequency, amplitude, file_path)
