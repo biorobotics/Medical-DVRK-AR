b894d18698c2c6983bccd887b548852bc3f443a8
anjali <anjali.pemmaraju@gmail.com>
2020-11-16 12:39:20 -0800

    Pulling some changes from alex's branch

--- src/medical_dvrk_ar/Modeling/stiffness_map.py
+++ src/medical_dvrk_ar/Modeling/stiffness_map.py
@@ -135,7 +135,7 @@ class stiffnessMap:
 		self.point_stiff = np.append(self.point_stiff, self.stiffNormalized, axis = 1)
 		# Append array that says whether a given point is part of a tumor or not
 		self.point_stiff = np.append(self.point_stiff, self.stiffness, axis = 1)
-		np.save('/home/anjalipemmaraju/catkin_ws/src/Medical-DVRK-AR/data/points_with_stiffness.npy', self.point_stiff)
+		np.save('../../../data/points_with_stiffness.npy', self.point_stiff)
 
 	
 	def visualizeStiffness(self):
