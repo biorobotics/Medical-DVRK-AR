49aa412a8762f183a4c65ff6932c291f8ec731fe
Alex Wu <chichiaw@andrew.cmu.edu>
2020-11-17 12:02:06 -0500

    passed by arguments

--- src/medical_dvrk_ar/integrated.py
+++ src/medical_dvrk_ar/integrated.py
@@ -9,69 +9,80 @@ import threading
 import numpy as np
 import rospy
 import time
+import argparse
 
-start_time = time.time()
-# im not sure if these are the right files right now because they're all named something different...
-file_path = "/home/alex/MRSD_sim/src/Medical-DVRK-AR/data"
 
-print("loading stiffness data")
-stiffnessMap = stiffnessMap(file_path+"xyz_for_stiffness_est.npy", 'h')
-stiffnessMap.getStiffnessMap()
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='data folder')
+    parser.add_argument('--path',help='the path to the data folder')
+    parser.add_argument('--method', help = 'which palpation method to use')
+    args = parser.parse_args()
 
-print("loading scanning path")
-data = np.load(file_path+"scanning_path_200_points.npy")
-# # turn this into argparse so that we can change amp and freq easily
-amplitude = 0.02 #0.02
-frequency = 0.5 #0.5
+    start_time = time.time()
+    # im not sure if these are the right files right now because they're all named something different...
+    # file_path = "/home/alex/MRSD_sim/src/Medical-DVRK-AR/data"
+    file_path = args.path
 
-finished = False
+    print("loading stiffness data")
+    stiffnessMap = stiffnessMap(file_path+"xyz_for_stiffness_est.npy", 'h')
+    stiffnessMap.getStiffnessMap()
 
-scanning_start_time = time.time()
-print("starting scan")
-scan_planner = Task_Planner(data, frequency, amplitude)
-finished = scan_planner.run()
-print("finished scan")
-scanning_end_time = time.time()
-print("Time used for scanning:", scanning_end_time- scanning_start_time)
-# # open blaser results and do pointcloud palpation filter somehow
+    print("loading scanning path")
+    data = np.load(file_path+"scanning_path_200_points.npy")
+    # # turn this into argparse so that we can change amp and freq easily
+    amplitude = 0.02 #0.02
+    frequency = 0.5 #0.5
 
-print("processing blaser results")
-processing_start_time = time.time()
-blaser_data_name = "blaser_results.npy"
-raw_data = np.load(file_path+blaser_data_name)
-max_angle = 60  # change  the param within [0,90)]
-keepRows = 200
-keepCols = 100
+    finished = False
 
-my_filter = filter_pointcloud_for_path_planner(max_angle, keepRows, keepCols, connectivity_test=True)
-a = my_filter.filter(raw_data, isPlyPath=False)
-np.save(file_path+"palpation_path_testing.npy", a)
-processing_end_time = time.time()
-print("Time used for data processing:", processing_end_time - processing_start_time)
+    scanning_start_time = time.time()
+    print("starting scan")
+    scan_planner = Task_Planner(data, frequency, amplitude)
+    finished = scan_planner.run()
+    print("finished scan")
+    scanning_end_time = time.time()
+    print("Time used for scanning:", scanning_end_time- scanning_start_time)
+    # # open blaser results and do pointcloud palpation filter somehow
 
-data = np.load(file_path + "palpation_path_testing.npy")
+    print("processing blaser results")
+    processing_start_time = time.time()
+    blaser_data_name = "blaser_results.npy"
+    raw_data = np.load(file_path+blaser_data_name)
+    max_angle = 60  # change  the param within [0,90)]
+    keepRows = 200
+    keepCols = 100
 
-print("beginning palpation")
-palpation_start_time = time.time()
+    my_filter = filter_pointcloud_for_path_planner(max_angle, keepRows, keepCols, connectivity_test=True)
+    a = my_filter.filter(raw_data, isPlyPath=False)
+    np.save(file_path+"palpation_path_testing.npy", a)
+    processing_end_time = time.time()
+    print("Time used for data processing:", processing_end_time - processing_start_time)
 
-##### skipping points algorithm #####
-#planner = Task_Planner_palpation(data, frequency, amplitude, file_path)
-#planner.run()
+    data = np.load(file_path + "palpation_path_testing.npy")
 
-##### skipping points algorithm #####
-gpr = gpr_palpation(data, frequency, amplitude, file_path, algorithm_name='UCB', visualize=False, simulation=False, wait_for_searching_signal = True) # 'LSE', 'EI', 'UCB'
-gpr.autoPalpation(100)
-palpation_end_time = time.time()
-print("Time used for palpation:", palpation_end_time - palpation_start_time)
+    print("beginning palpation")
+    palpation_start_time = time.time()
 
-ground_truth_stiffness = np.load('../../data/points_with_stiffness.npy')
-print(ground_truth_stiffness.shape)
-palpated_map = np.load('../../data/palpation_result.npy')
-print(palpated_map.shape)
-compute_accuracy(ground_truth_stiffness, palpated_map)
-end_time = time.time()
+    if args.method == 'naive':
+        ##### skipping points algorithm #####
+        planner = Task_Planner_palpation(data, frequency, amplitude, file_path)
+        planner.run()
+    else:
+        ##### skipping points algorithm #####
+        gpr = gpr_palpation(data, frequency, amplitude, file_path, algorithm_name='UCB', visualize=False, simulation=False, wait_for_searching_signal = True) # 'LSE', 'EI', 'UCB'
+        gpr.autoPalpation(100)
 
-print("Time used for scanning:", scanning_end_time- scanning_start_time)
-print("Time used for data processing:", processing_end_time - processing_start_time)
-print("Time used for palpation:", palpation_end_time - palpation_start_time)
-print("Total time used:", end_time - start_time)
+    palpation_end_time = time.time()
+    print("Time used for palpation:", palpation_end_time - palpation_start_time)
+
+    ground_truth_stiffness = np.load('../../data/points_with_stiffness.npy')
+    print(ground_truth_stiffness.shape)
+    palpated_map = np.load('../../data/palpation_result.npy')
+    print(palpated_map.shape)
+    compute_accuracy(ground_truth_stiffness, palpated_map)
+    end_time = time.time()
+
+    print("Time used for scanning:", scanning_end_time- scanning_start_time)
+    print("Time used for data processing:", processing_end_time - processing_start_time)
+    print("Time used for palpation:", palpation_end_time - palpation_start_time)
+    print("Total time used:", end_time - start_time)
